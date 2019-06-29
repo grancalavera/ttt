cfb08901e4feb37f57829f64750b8090f66de5ca
Leon Coto <leoncoto@gmail.com>
2019-06-29 22:16:46 +0100

    ensure dependencies are linked correclty

--- ttt-client/src/App.tsx
+++ ttt-client/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { board } from "@grancalavera/ttt-core";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>{board.join(" | ")}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +23,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
