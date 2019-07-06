0812a4aab5ee8e44839a0c0d21615b7b8ffbbe65
Leon Coto <leoncoto@gmail.com>
2019-07-06 20:51:30 +0100

    add more coments and cleanup a bit

--- ttt-server/src/data-sources/user.ts
+++ ttt-server/src/data-sources/user.ts
@@ -11,26 +11,33 @@ export class UserAPI extends DataSource<Context> {
   }
 
   async findOrCreateUser(maybeLoginArgs?: MutationLoginArgs) {
-    const findOrCreateOrNull = () =>
-      maybeLoginArgs ? inStore_findOrCreateUser(maybeLoginArgs) : null;
-
-    // We need to check for kind in runtime because context
+    // We need to check for `kind` in runtime because context
     // is created by other means besides the context parameter
     // to the apollo constructor. For instance `dataSources`
-    // are added to the context as well...
+    // are added to the context as well. When the context parameter
+    // hasn't been provider, we get a context, but *not* the context
+    // in the type signature.
     if (this.context.kind) {
       switch (this.context.kind) {
-        // no idea why we should end up here... if we're logged in
-        // why would we try to log in again?
         case "LoggedIn":
+          // we end up here when the request has an `authorization` header
           return inStore_findOrCreateUser(this.context.user);
         case "LoggedOut":
-          return findOrCreateOrNull();
+          // we end up here either when:
+          // 1. there is no authorization header and someone
+          //    is querying `me` (for instance), or
+          // 2. there is no authorization header ans someone
+          //    is mutating `login` (for instance)
+          return maybeLoginArgs ? inStore_findOrCreateUser(maybeLoginArgs) : null;
         default:
           return assertNever(this.context);
       }
     } else {
-      return findOrCreateOrNull();
+      // this is just and odd case, might happen as above, when the
+      // context function hasn't been assigned to the apollo server
+      // parameters in the constructor. no need to worry I think,
+      // just do noting and return null
+      return null;
     }
   }
 }
@@ -38,7 +45,11 @@ export class UserAPI extends DataSource<Context> {
 const inStore_findOrCreateUser = async (user: MutationLoginArgs): Promise<User> => {
   const { alias, email } = user;
   const [userModel] = await UserModel.findOrCreate({ where: { alias, email } });
-  return { id: userModel.id.toString(), alias: userModel.alias, email: userModel.email };
+  return {
+    id: userModel.id.toString(),
+    alias: userModel.alias,
+    email: userModel.email
+  };
 };
 
 function assertNever(x: never): never {
