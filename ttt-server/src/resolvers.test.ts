dd47582e67c77578859f9ce0feb81f5660c40242
Leon Coto <leoncoto@gmail.com>
2019-08-01 08:22:56 +0100

    write better test descriptions

--- ttt-server/src/resolvers.test.ts
+++ ttt-server/src/resolvers.test.ts
@@ -1,11 +1,37 @@
-describe("joining new games", () => {
-  describe("when there are NO GAMES", () => {
-    it.todo("a player joining creates a new game");
-    it.todo("the first game created has ONE player in the lobby");
-    it.todo("the first game crated state's should be GameLobby");
+import { create as createStore, UserModel } from "./store";
+
+describe("Alice, Bob and Jane are the first users to ever join a game.", () => {
+  const storage = "./join-game.sqlite";
+
+  let alice: UserModel;
+  let bob: UserModel;
+  let jane: UserModel;
+
+  beforeAll(async () => {
+    await createStore({ storage }).sync();
+    [alice] = await UserModel.findOrCreate({ where: { email: "alice@email.com" } });
+    [bob] = await UserModel.findOrCreate({ where: { email: "bob@email.com" } });
+    [jane] = await UserModel.findOrCreate({ where: { email: "jane@email.com" } });
   });
 
-  describe("when there is ONE GAME at the lobby", () => {
+  describe("Alice joins the first game ever, then:", () => {
+    it.todo("the game's state should be 'GameLobby'");
+    it.todo("Alice should be the player 'waiting' at the lobby");
+  });
+
+  describe("Alice joins another game, then:", () => {
+    it.todo("the game's state should be 'GameLobby'");
+    it.todo("Alice should be the player 'waiting' at the lobby of all existing games");
+  });
+
+  describe("Bob wants to join a game", () => {
+    it.todo("a player can join the game when she's not already in the same game");
+    it.todo(
+      "a player should create a new game if she's already waiting in the only existing game"
+    );
+  });
+
+  describe("Jane wants to join a game", () => {
     it.todo("a player can join the game when she's not already in the same game");
     it.todo(
       "a player should create a new game if she's already waiting in the only existing game"
