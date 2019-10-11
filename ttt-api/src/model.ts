534d12bc359f74e9ac006b866907f3e800b110ae
Leon Coto <leoncoto@gmail.com>
2019-10-11 15:56:41 +0100

    remove dead code

--- ttt-api/src/model.ts
+++ ttt-api/src/model.ts
@@ -1,28 +1,4 @@
-import { CoreGame, CoreMove, CorePlayer, CorePosition } from "@grancalavera/ttt-core";
-
-export enum ErrorCode {
-  NotFound = "the requested game does not exist",
-  GameOver = "game over: the requested game is over",
-  WrongTurn = "wrong player: not this player's turn",
-  WrongMove = "wrong move: position already taken",
-  InvalidMove = "invalid move: either the Player or the Position are invalid"
-}
-
-export type GameResponse = {
-  id: string;
-  game: CoreGame;
-};
-
-export type MovesResponse = {
-  id: string;
-  moves: CoreMove[];
-};
-
-export type ErrorResponse = {
-  code: ErrorCode;
-  message: string;
-  context: any;
-};
+import { CoreMove, CorePlayer, CorePosition } from "@grancalavera/ttt-core";
 
 export type ResponseGame = {
   id: string;
