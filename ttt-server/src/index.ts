6495a21b8f298b1815d11960e99b5a93f9ccd349
Leon Coto <leoncoto@gmail.com>
2019-11-08 08:34:20 +0000

    new server setup ala ben awad

--- ttt-server/src/index.ts
+++ ttt-server/src/index.ts
@@ -1,32 +1,66 @@
-import { ApolloServer, makeExecutableSchema } from "apollo-server";
-import { dataSources, context } from "./environment";
-import { resolvers } from "./resolvers";
-import * as store from "./store";
-import { importSchema } from "graphql-import";
-import { join } from "path";
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers/user";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const typeDefs = importSchema(join(__dirname, "schema.graphql"));
+const port = 4000;
 
-const schema = makeExecutableSchema({
-  // this should be fine because we're adding __typename to all members of union types
-  // also, I didn't found a way to implement a __resolveType function that would work
-  // when the parent is a Promise
-  resolverValidationOptions: { requireResolversForResolveType: false },
-  typeDefs,
-  resolvers
-});
-
-const server = new ApolloServer({
-  schema,
-  dataSources,
-  context
-});
-
-store
-  .create({ storage: "./store.sqlite" })
-  .sync({ force: true })
-  .then(() => server.listen())
-  .then(({ url, subscriptionsUrl }) => {
-    console.log(`🚀  Server ready at ${url}`);
-    console.log(`🚀  Subscriptions ready at ${subscriptionsUrl}`);
+(async () => {
+  const app = express();
+  app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+  app.use(cookieParser());
+  app.get("/", (_, res) => {
+    res.send(`
+<a href="/graphql">click here to install all the viruses</a>
+    `);
   });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [UserResolver] }),
+    context: ({ req, res }) => ({ req, res })
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  app.listen(port, () => {
+    console.log(`express + graphql server running at port ${port}`);
+  });
+})();
+
+// import { ApolloServer, makeExecutableSchema } from "apollo-server";
+// import { dataSources, context } from "./environment";
+// import { resolvers } from "./resolvers";
+// import * as store from "./store";
+// import { importSchema } from "graphql-import";
+// import { join } from "path";
+
+// const typeDefs = importSchema(join(__dirname, "schema.graphql"));
+
+// const schema = makeExecutableSchema({
+//   // this should be fine because we're adding __typename to all members of union types
+//   // also, I didn't found a way to implement a __resolveType function that would work
+//   // when the parent is a Promise
+//   resolverValidationOptions: { requireResolversForResolveType: false },
+//   typeDefs,
+//   resolvers
+// });
+
+// const server = new ApolloServer({
+//   schema,//     console.log(`🚀  Server ready at ${url}`);
+//     console.log(`🚀  Subscriptions ready at ${subscriptionsUrl}`);
+
+//   dataSources,
+//   context
+// });
+
+// store
+//   .create({ storage: "./store.sqlite" })
+//   .sync({ force: true })
+//   .then(() => server.listen())
+//   .then(({ url, subscriptionsUrl }) => {
+//     console.log(`🚀  Server ready at ${url}`);
+//     console.log(`🚀  Subscriptions ready at ${subscriptionsUrl}`);
+//   });
--- ttt-server/src/resolvers/user.ts
+++ ttt-server/src/resolvers/user.ts
@@ -1,42 +1,52 @@
-import { TTTContext } from "../environment";
-import { LoginResult, User } from "../generated/models";
-import { userFromModel } from "../model";
-import { pubsub, PUBSUB_USER_CREATED } from "../pubsub";
-import uuid from "uuid";
+import { Query, Resolver } from "type-graphql";
 
-export const getMe = (context: TTTContext): User | null => {
-  const { userStatus } = context;
-  return userStatus.isLoggedIn ? userStatus.user : null;
-};
-
-export const login = async (
-  context: TTTContext,
-  id: string | null | undefined
-): Promise<LoginResult> => {
-  const {
-    dataSources: { gameStore },
-    userStatus
-  } = context;
-
-  if (userStatus.isLoggedIn) {
-    return {
-      user: { id: userStatus.user.id },
-      created: false
-    };
-  } else {
-    const { userModel, created } = await gameStore.findOrCreateUser(id || uuid());
-    if (created) {
-      pubsub.publish(PUBSUB_USER_CREATED, { userCreated: userFromModel(userModel) });
-    }
-    return {
-      user: { id: userModel.id },
-      created
-    };
+@Resolver()
+export class UserResolver {
+  @Query(() => String)
+  hello() {
+    return "hi";
   }
-};
+}
 
-export const getAllUsers = async (context: TTTContext) => {
-  const userModels = await context.dataSources.gameStore.findAllUsers();
-  const users = userModels.map(userFromModel);
-  return users;
-};
+// import { TTTContext } from "../environment";
+// import { LoginResult, User } from "../generated/models";
+// import { userFromModel } from "../model";
+// import { pubsub, PUBSUB_USER_CREATED } from "../pubsub";
+// import uuid from "uuid";
+
+// export const getMe = (context: TTTContext): User | null => {
+//   const { userStatus } = context;
+//   return userStatus.isLoggedIn ? userStatus.user : null;
+// };
+
+// export const login = async (
+//   context: TTTContext,
+//   id: string | null | undefined
+// ): Promise<LoginResult> => {
+//   const {
+//     dataSources: { gameStore },
+//     userStatus
+//   } = context;
+
+//   if (userStatus.isLoggedIn) {
+//     return {
+//       user: { id: userStatus.user.id },
+//       created: false
+//     };
+//   } else {
+//     const { userModel, created } = await gameStore.findOrCreateUser(id || uuid());
+//     if (created) {
+//       pubsub.publish(PUBSUB_USER_CREATED, { userCreated: userFromModel(userModel) });
+//     }
+//     return {
+//       user: { id: userModel.id },
+//       created
+//     };
+//   }
+// };
+
+// export const getAllUsers = async (context: TTTContext) => {
+//   const userModels = await context.dataSources.gameStore.findAllUsers();
+//   const users = userModels.map(userFromModel);
+//   return users;
+// };
