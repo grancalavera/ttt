85e7b4c311f3278b7538dc6f3eccf4d163fd13cb
Leon Coto <leoncoto@gmail.com>
2019-11-09 17:27:51 +0000

    prep for apollo and stuff

--- ttt-client/src/App.tsx
+++ ttt-client/src/App.tsx
@@ -1,3 +1,7 @@
 import React from "react";
+import { Button } from "@blueprintjs/core";
+import "@blueprintjs/core/lib/css/blueprint.css";
 
-export const App: React.FC = () => <>Nothing</>;
+export const App: React.FC = () => {
+  return <Button intent="danger" text="install all the viruses" />;
+};
