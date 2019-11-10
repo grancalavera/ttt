e64209e3a0d66c381f5083c48393df9ada3647ac
Leon Coto <leoncoto@gmail.com>
2019-11-10 14:25:14 +0000

    more wip than anything else

--- ttt-client/src/App.tsx
+++ ttt-client/src/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
 import { Button } from "@blueprintjs/core";
 import "@blueprintjs/core/lib/css/blueprint.css";
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
 export const App: React.FC = () => {
-  return <Button intent="danger" text="install all the viruses" />;
+  const PING = gql`
+    query Ping {
+      ping
+    }
+  `;
+
+  const { data, loading } = useQuery(PING);
+
+  return (
+    <>
+      {loading ? <p>loading...</p> : <pre>{JSON.stringify(data, null, 2)}</pre>}
+      <Button intent="danger" text="install all the viruses" />
+    </>
+  );
 };
--- ttt-client/src/index.tsx
+++ ttt-client/src/index.tsx
@@ -1,5 +1,18 @@
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "http://localhost:4000/graphql"
+  }),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
