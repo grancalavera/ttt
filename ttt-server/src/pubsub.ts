916b8e986ddb2d397f84aeb5b96cb9b7aa64dae2
Leon Coto <leoncoto@gmail.com>
2019-11-12 21:16:21 +0000

    remove old code

--- ttt-server/src/pubsub.ts
+++
@@ -1,8 +0,0 @@
-import { PubSub } from "apollo-server";
-
-export const PUBSUB_GAME_ADDED = "PUBSUB_GAME_ADDED";
-export const PUBSUB_GAME_CHANGED = "PUBSUB_GAME_CHANGED";
-export const PUBSUB_USER_CREATED = "PUBSUB_USER_CREATED";
-
-// TODO: move to datasource
-export const pubsub = new PubSub();
--- ttt-server/src/resolvers/get-all-games.ts
+++
@@ -1,7 +0,0 @@
-import { GameResponse } from "@grancalavera/ttt-api";
-import { TTTContext } from "../environment";
-import { Game } from "../generated/models";
-
-export const getAllGames = async (context: TTTContext): Promise<Game[]> => {
-  throw new Error("getAllGames is not implemented");
-};
