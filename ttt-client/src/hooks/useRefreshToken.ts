59d09c8ea4b19d6cc5abee766e78e4e352d5216b
Leon Coto <leoncoto@gmail.com>
2019-11-17 19:55:17 +0000

    add user to secure resolver and remove auto registration

--- ttt-client/src/hooks/useRefreshToken.ts
+++ ttt-client/src/hooks/useRefreshToken.ts
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { setAccessToken } from "../access-token";
 
+export type AuthStatus = "loading" | "authorized" | "unauthorized";
+
 export const useRefreshToken = () => {
-  const [loading, setLoading] = useState(true);
+  const [authStatus, setAuthStatus] = useState<AuthStatus>("loading");
   useEffect(() => {
     (async () => {
       const response = await fetch("http://localhost:4000/refresh_token", {
@@ -13,9 +15,11 @@ export const useRefreshToken = () => {
       setAccessToken(accessToken);
       if (response.status !== 200) {
         console.error("failed to refresh token!");
+        setAuthStatus("unauthorized");
+      } else {
+        setAuthStatus("authorized");
       }
-      setLoading(false);
     })();
   }, []);
-  return loading;
+  return authStatus;
 };
