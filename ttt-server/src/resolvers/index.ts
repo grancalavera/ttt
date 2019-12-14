f5f151801e56b948aa4043a0a4befca3d17d6e06
Leon Coto <leoncoto@gmail.com>
2019-12-14 13:57:46 +0000

    add game state to my games query

--- ttt-server/src/resolvers/index.ts
+++ ttt-server/src/resolvers/index.ts
@@ -1,7 +1,11 @@
 import * as game from "resolvers/game";
 import * as users from "resolvers/users";
 import * as util from "resolvers/util";
-import { MutationResolvers, QueryResolvers } from "../generated/graphql";
+import {
+  MutationResolvers,
+  QueryResolvers,
+  GameStateResolvers,
+} from "../generated/graphql";
 
 const Query: QueryResolvers = {
   users: (_, __, ctx) => users.list(ctx),
@@ -10,10 +14,21 @@ const Query: QueryResolvers = {
   ping: util.ping,
 };
 
+const GameState: GameStateResolvers = {
+  __resolveType: ({ __typename }) => enforceTypename(__typename),
+};
+
 const Mutation: MutationResolvers = {
   register: (_, __, ctx) => users.register(ctx),
   join: async (_, __, ctx) => ctx.secure(game.join(ctx)),
   play: (_, { input }, ctx) => ctx.secure(game.play(ctx, input)),
 };
 
-export const resolvers = { Query, Mutation };
+export const resolvers = { Query, Mutation, GameState };
+
+const enforceTypename = <T extends string>(typename?: T): T => {
+  if (typename) {
+    return typename;
+  }
+  throw new Error("missing required `__typename`");
+};
