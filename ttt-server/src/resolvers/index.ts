f687bd5f5a5ddeef39f2b7d98973edfafa545c4d
Leon Coto <leoncoto@gmail.com>
2019-12-14 12:29:57 +0000

    fix types for listing my games

--- ttt-server/src/resolvers/index.ts
+++ ttt-server/src/resolvers/index.ts
@@ -1,15 +1,12 @@
-import { MutationResolvers, QueryResolvers } from "../generated/graphql";
 import * as game from "resolvers/game";
 import * as users from "resolvers/users";
 import * as util from "resolvers/util";
-import { Context } from "context";
-
-const myGames = (a: any, b: any, ctx: any) => ctx.secure(game.myGames(ctx));
+import { MutationResolvers, QueryResolvers } from "../generated/graphql";
 
 const Query: QueryResolvers = {
   users: (_, __, ctx) => users.list(ctx),
   whoami: (_, __, ctx) => ctx.secure(util.whoami),
-  myGames: myGames,
+  myGames: (_, __, ctx) => ctx.secure(game.myGames(ctx)),
   ping: util.ping,
 };
 
