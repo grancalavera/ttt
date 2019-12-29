622f1e1010f3976e3aa35880be6bfabf7e68e6f1
Leon Coto <leoncoto@gmail.com>
2019-12-29 11:17:00 +0000

    cleanup

--- ttt-client/src/app-context.tsx
+++ ttt-client/src/app-context.tsx
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
+import { useAuthentication } from "./hooks/use-authentication";
 
 export interface Context {
   loading: boolean;
+  authenticated: boolean;
   setLoading: (value: boolean) => void;
 }
 
 export const AppContext = React.createContext<Context>({
   loading: false,
+  authenticated: false,
   setLoading: () => {},
 });
 
 export const TTTApp: React.FC = ({ children }) => {
   const [loading, setLoading] = useState(false);
+  const authenticated = useAuthentication();
 
   return (
-    <AppContext.Provider value={{ loading, setLoading }}>
+    <AppContext.Provider value={{ loading, setLoading, authenticated }}>
       {children}
     </AppContext.Provider>
   );
