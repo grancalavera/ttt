05772c3208d4b7a262bd69e22fcb1f95db727a46
Leon Coto <leoncoto@gmail.com>
2019-12-30 18:29:34 +0000

    hide composed call for set loading in a hook

--- ttt-client/src/app-context.tsx
+++ ttt-client/src/app-context.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { useAuthentication } from "./hooks/use-authentication";
+import { ActivityState, isLoading } from "./common/activity-state";
+import { useCallback } from "react";
 
 export interface Context {
   loading: boolean;
   authenticated: boolean;
   setLoading: (value: boolean) => void;
+  setLoadingFromActivity: (activity: ActivityState<any>) => void;
 }
 
 export const AppContext = React.createContext<Context>({
@@ -13,14 +16,26 @@ export const AppContext = React.createContext<Context>({
   setLoading: () => {
     throw new Error("setLoading is not implemented");
   },
+  setLoadingFromActivity: () => {
+    throw new Error("setLoadingFromActivity is not implemented");
+  },
 });
 
 export const AppContextProvider: React.FC = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const authenticated = useAuthentication();
 
+  const setLoadingFromActivity = useCallback(
+    (a: ActivityState<any>) => {
+      setLoading(isLoading(a));
+    },
+    [setLoading]
+  );
+
   return (
-    <AppContext.Provider value={{ loading, setLoading, authenticated }}>
+    <AppContext.Provider
+      value={{ loading, setLoading, authenticated, setLoadingFromActivity }}
+    >
       {children}
     </AppContext.Provider>
   );
