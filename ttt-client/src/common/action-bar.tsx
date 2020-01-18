3990df256c8956d059adda294ac9d1c1eac101e1
Leon Coto <leoncoto@gmail.com>
2020-01-18 19:50:08 +0000

    improve action bar component

--- ttt-client/src/common/action-bar.tsx
+++ ttt-client/src/common/action-bar.tsx
@@ -1,50 +1,71 @@
 import { Alignment, Button, Navbar } from "@blueprintjs/core";
-import React, { useCallback, useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { getAccessToken } from "../access-token";
 import { AppContext } from "../app-context";
 import { WhoAmI } from "./who-am-i";
 
-const copyGraphQLSettings = (o: any) => {
+const copyToClipboard = (o: Object | undefined) => () => {
+  if (o === undefined) {
+    return;
+  }
+
   const result = JSON.stringify(o, null, 2);
   console.log(result);
   navigator.clipboard.writeText(result);
 };
 
 export const ActionBar: React.FC = () => {
-  const { token, gameId } = useContext(AppContext);
+  const { token, gameId, authenticated } = useContext(AppContext);
+  const [authHeader, setAuthHeader] = useState<Object | undefined>();
+  const [gameStatusVars, setGameStatusVars] = useState<Object | undefined>("");
+  const [playVars, setPlayVars] = useState<Object | undefined>();
 
-  const copyAccessTokenHeader = useCallback(() => {
-    copyGraphQLSettings({ authorization: `bearer ${getAccessToken()}` });
-  }, []);
+  useEffect(() => {
+    if (authenticated) {
+      setAuthHeader({ authorization: `bearer ${getAccessToken()}` });
+    }
 
-  const copyGameStatusParams = useCallback(() => {
-    copyGraphQLSettings({ gameId });
-  }, [gameId]);
+    if (gameId) {
+      setGameStatusVars({ gameId });
+    }
 
-  const copyPlayParams = useCallback(() => {
-    copyGraphQLSettings({
-      input: {
-        gameId,
-        token,
-        position: "A",
-      },
-    });
-  }, [gameId, token]);
+    if (gameId && token) {
+      setPlayVars({
+        input: {
+          gameId,
+          token,
+          position: "A",
+        },
+      });
+    }
+  }, [authenticated, setAuthHeader, gameId, setGameStatusVars, token, setPlayVars]);
 
   return (
     <Navbar fixedToTop>
       <Navbar.Group align={Alignment.LEFT}>
-        <WhoAmI />
-        <Navbar.Divider />
-        <Button minimal={true} onClick={copyAccessTokenHeader}>
+        <Button
+          minimal={true}
+          disabled={!authHeader}
+          onClick={copyToClipboard(authHeader)}
+        >
           Copy Authorization Header
         </Button>
-        <Button minimal={true} onClick={copyGameStatusParams}>
-          Copy Game Status Variables
+
+        <Button
+          minimal={true}
+          disabled={!gameStatusVars}
+          onClick={copyToClipboard(gameStatusVars)}
+        >
+          Copy GameStatus Variables
         </Button>
-        <Button minimal={true} onClick={copyPlayParams}>
+
+        <Button minimal={true} disabled={!playVars} onClick={copyToClipboard(playVars)}>
           Copy Play Variables
         </Button>
+
+        <Navbar.Divider />
+
+        <WhoAmI />
       </Navbar.Group>
     </Navbar>
   );
