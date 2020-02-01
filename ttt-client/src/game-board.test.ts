49141bc9d19c63970f0e4674dfe916c95cafdae8
Leon Coto <leoncoto@gmail.com>
2020-02-01 18:38:04 +0000

    update test to allow preventing plays in others turn

--- ttt-client/src/game-board.test.ts
+++ ttt-client/src/game-board.test.ts
@@ -1,6 +1,14 @@
 import { assertNever } from "@grancalavera/ttt-core";
 import { CellState, updateBoard } from "./game-board";
-import { GameStatus, Move, Position, Token } from "./generated/graphql";
+import {
+  GameStatus,
+  Move,
+  Position,
+  Token,
+  GamePlaying,
+  GameWon,
+  GameDraw,
+} from "./generated/graphql";
 
 interface Scenario {
   name: string;
@@ -8,27 +16,52 @@ interface Scenario {
   expected: CellState[];
 }
 
+const PLAYING_NEXT_O_ME_O: GamePlaying = {
+  __typename: "GamePlaying",
+  id: "0",
+  me: Token.O,
+  moves: [],
+  next: Token.O,
+};
+
+const PLAYING_NEXT_X_ME_O: GamePlaying = {
+  __typename: "GamePlaying",
+  id: "0",
+  me: Token.O,
+  moves: [],
+  next: Token.X,
+};
+
+const WON: GameWon = {
+  __typename: "GameWon",
+  id: "0",
+  me: Token.O,
+  winner: Token.O,
+  moves: [],
+};
+
+const DRAW: GameDraw = {
+  __typename: "GameDraw",
+  id: "0",
+  me: Token.O,
+  moves: [],
+};
+
 const scenarios: Scenario[] = [
-  mkScenario(
-    "empty moves",
-    "GamePlaying",
-    [],
-    [
-      { kind: "free", move: { position: Position.A, token: Token.O } },
-      { kind: "free", move: { position: Position.B, token: Token.O } },
-      { kind: "free", move: { position: Position.C, token: Token.O } },
-      { kind: "free", move: { position: Position.D, token: Token.O } },
-      { kind: "free", move: { position: Position.E, token: Token.O } },
-      { kind: "free", move: { position: Position.F, token: Token.O } },
-      { kind: "free", move: { position: Position.G, token: Token.O } },
-      { kind: "free", move: { position: Position.H, token: Token.O } },
-      { kind: "free", move: { position: Position.I, token: Token.O } },
-    ]
-  ),
+  mkScenario("empty moves", PLAYING_NEXT_O_ME_O, [
+    { kind: "free", move: { position: Position.A, token: Token.O } },
+    { kind: "free", move: { position: Position.B, token: Token.O } },
+    { kind: "free", move: { position: Position.C, token: Token.O } },
+    { kind: "free", move: { position: Position.D, token: Token.O } },
+    { kind: "free", move: { position: Position.E, token: Token.O } },
+    { kind: "free", move: { position: Position.F, token: Token.O } },
+    { kind: "free", move: { position: Position.G, token: Token.O } },
+    { kind: "free", move: { position: Position.H, token: Token.O } },
+    { kind: "free", move: { position: Position.I, token: Token.O } },
+  ]),
   mkScenario(
     "first cell taken",
-    "GamePlaying",
-    [{ position: Position.A, token: Token.O }],
+    { ...PLAYING_NEXT_O_ME_O, moves: [{ position: Position.A, token: Token.O }] },
     [
       { kind: "played", move: { position: Position.A, token: Token.O } },
       { kind: "free", move: { position: Position.B, token: Token.O } },
@@ -43,11 +76,13 @@ const scenarios: Scenario[] = [
   ),
   mkScenario(
     "two non consecutive ordered cells taken",
-    "GamePlaying",
-    [
-      { position: Position.A, token: Token.O },
-      { position: Position.C, token: Token.O },
-    ],
+    {
+      ...PLAYING_NEXT_O_ME_O,
+      moves: [
+        { position: Position.A, token: Token.O },
+        { position: Position.C, token: Token.O },
+      ],
+    },
     [
       { kind: "played", move: { position: Position.A, token: Token.O } },
       { kind: "free", move: { position: Position.B, token: Token.O } },
@@ -62,11 +97,13 @@ const scenarios: Scenario[] = [
   ),
   mkScenario(
     "two non consecutive non ordered cells taken",
-    "GamePlaying",
-    [
-      { position: Position.C, token: Token.O },
-      { position: Position.A, token: Token.O },
-    ],
+    {
+      ...PLAYING_NEXT_O_ME_O,
+      moves: [
+        { position: Position.C, token: Token.O },
+        { position: Position.A, token: Token.O },
+      ],
+    },
     [
       { kind: "played", move: { position: Position.A, token: Token.O } },
       { kind: "free", move: { position: Position.B, token: Token.O } },
@@ -81,12 +118,14 @@ const scenarios: Scenario[] = [
   ),
   mkScenario(
     "three consecutive ordered cells taken",
-    "GamePlaying",
-    [
-      { position: Position.A, token: Token.O },
-      { position: Position.B, token: Token.O },
-      { position: Position.C, token: Token.O },
-    ],
+    {
+      ...PLAYING_NEXT_O_ME_O,
+      moves: [
+        { position: Position.A, token: Token.O },
+        { position: Position.B, token: Token.O },
+        { position: Position.C, token: Token.O },
+      ],
+    },
     [
       { kind: "played", move: { position: Position.A, token: Token.O } },
       { kind: "played", move: { position: Position.B, token: Token.O } },
@@ -101,12 +140,14 @@ const scenarios: Scenario[] = [
   ),
   mkScenario(
     "three non consecutive non ordered cells taken",
-    "GamePlaying",
-    [
-      { position: Position.C, token: Token.O },
-      { position: Position.A, token: Token.O },
-      { position: Position.B, token: Token.O },
-    ],
+    {
+      ...PLAYING_NEXT_O_ME_O,
+      moves: [
+        { position: Position.C, token: Token.O },
+        { position: Position.A, token: Token.O },
+        { position: Position.B, token: Token.O },
+      ],
+    },
     [
       { kind: "played", move: { position: Position.A, token: Token.O } },
       { kind: "played", move: { position: Position.B, token: Token.O } },
@@ -121,18 +162,20 @@ const scenarios: Scenario[] = [
   ),
   mkScenario(
     "all cells taken",
-    "GamePlaying",
-    [
-      { position: Position.A, token: Token.O },
-      { position: Position.B, token: Token.O },
-      { position: Position.C, token: Token.O },
-      { position: Position.D, token: Token.O },
-      { position: Position.E, token: Token.O },
-      { position: Position.F, token: Token.O },
-      { position: Position.G, token: Token.O },
-      { position: Position.H, token: Token.O },
-      { position: Position.I, token: Token.O },
-    ],
+    {
+      ...PLAYING_NEXT_O_ME_O,
+      moves: [
+        { position: Position.A, token: Token.O },
+        { position: Position.B, token: Token.O },
+        { position: Position.C, token: Token.O },
+        { position: Position.D, token: Token.O },
+        { position: Position.E, token: Token.O },
+        { position: Position.F, token: Token.O },
+        { position: Position.G, token: Token.O },
+        { position: Position.H, token: Token.O },
+        { position: Position.I, token: Token.O },
+      ],
+    },
     [
       { kind: "played", move: { position: Position.A, token: Token.O } },
       { kind: "played", move: { position: Position.B, token: Token.O } },
@@ -147,12 +190,14 @@ const scenarios: Scenario[] = [
   ),
   mkScenario(
     "draw",
-    "GameDraw",
-    [
-      { position: Position.C, token: Token.O },
-      { position: Position.A, token: Token.O },
-      { position: Position.B, token: Token.O },
-    ],
+    {
+      ...DRAW,
+      moves: [
+        { position: Position.C, token: Token.O },
+        { position: Position.A, token: Token.O },
+        { position: Position.B, token: Token.O },
+      ],
+    },
     [
       { kind: "played", move: { position: Position.A, token: Token.O } },
       { kind: "played", move: { position: Position.B, token: Token.O } },
@@ -167,12 +212,14 @@ const scenarios: Scenario[] = [
   ),
   mkScenario(
     "won",
-    "GameWon",
-    [
-      { position: Position.C, token: Token.O },
-      { position: Position.A, token: Token.O },
-      { position: Position.B, token: Token.O },
-    ],
+    {
+      ...WON,
+      moves: [
+        { position: Position.C, token: Token.O },
+        { position: Position.A, token: Token.O },
+        { position: Position.B, token: Token.O },
+      ],
+    },
     [
       { kind: "played", move: { position: Position.A, token: Token.O } },
       { kind: "played", move: { position: Position.B, token: Token.O } },
@@ -195,30 +242,10 @@ describe.each(scenarios)("generateBoard", scenario => {
   });
 });
 
-type Typename = Exclude<GameStatus["__typename"], undefined>;
-
-function mkScenario(
-  name: string,
-  __typename: Typename = "GamePlaying",
-  moves: Move[],
-  expected: CellState[]
-): Scenario {
+function mkScenario(name: string, game: GameStatus, expected: CellState[]): Scenario {
   return {
     name,
-    game: getGame(__typename, moves),
+    game,
     expected,
   };
-}
-
-function getGame(__typename: Typename, moves: Move[]): GameStatus {
-  switch (__typename) {
-    case "GamePlaying":
-      return { __typename, id: "0", me: Token.O, next: Token.O, moves };
-    case "GameDraw":
-      return { __typename, id: "0", me: Token.O, moves };
-    case "GameWon":
-      return { __typename, id: "0", me: Token.O, winner: Token.O, moves };
-    default:
-      return assertNever(__typename);
-  }
 }
