6427b5c7275a1460945a4fa8662392a8efcc76d5
Leon Coto <leoncoto@gmail.com>
2020-02-07 08:34:10 +0000

    parameterize initial loading map state
    using a ref so that it doesn't trigger re-renders

--- ttt-client/src/hooks/use-loader.tsx
+++ ttt-client/src/hooks/use-loader.tsx
@@ -1,33 +1,41 @@
 import React, { useCallback, useContext, useEffect, useMemo, useState } from "react";
+import { useRef } from "react";
 
 const LoaderContext = React.createContext({
   isLoading: false,
   setLoading: (value: boolean): void => {
     throw new Error("LoaderContext.setLoading is not implemented");
   },
+  loadingMap: new Map() as Map<symbol, true>,
 });
 
-export const LoaderContextProvider: React.FC = ({ children }) => {
+type Props = { loadingMap?: Map<symbol, true> };
+
+export const LoaderContextProvider: React.FC<Props> = ({
+  children,
+  loadingMap = new Map(),
+}) => {
   const [isLoading, setLoading] = useState(false);
   return (
-    <LoaderContext.Provider value={{ isLoading, setLoading }}>
+    <LoaderContext.Provider value={{ isLoading, setLoading, loadingMap }}>
       {children}
     </LoaderContext.Provider>
   );
 };
 
 export const useLoader = () => {
-  const { isLoading, setLoading } = useContext(LoaderContext);
+  const { isLoading, setLoading, loadingMap } = useContext(LoaderContext);
   const id = useIdentity();
+  const mapRef = useRef(loadingMap);
 
   const showLoader = useCallback(() => {
-    loadingMap.set(id, true);
+    mapRef.current.set(id, true);
     setLoading(true);
   }, [id, setLoading]);
 
   const hideLoader = useCallback(() => {
-    loadingMap.delete(id);
-    if (loadingMap.size === 0) {
+    mapRef.current.delete(id);
+    if (mapRef.current.size === 0) {
       setLoading(false);
     }
   }, [id, setLoading]);
@@ -36,7 +44,7 @@ export const useLoader = () => {
 
   const toggleLoader = useCallback(
     (show: boolean) => {
-      const isKnown = loadingMap.get(id) ?? false;
+      const isKnown = mapRef.current.get(id) ?? false;
 
       if (show && !isKnown) {
         showLoader();
@@ -50,5 +58,4 @@ export const useLoader = () => {
   return { isLoading, toggleLoader };
 };
 
-const loadingMap = new Map<symbol, true>();
 const useIdentity = () => useMemo(() => Symbol(), []);
