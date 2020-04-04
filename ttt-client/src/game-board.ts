d8ec671ded82e00acfc4ad5b0401a26c13becbf8
Leon Coto <leoncoto@gmail.com>
2020-04-04 13:32:30 +0100

    bring client back to life

--- ttt-client/src/game-board.ts
+++ ttt-client/src/game-board.ts
@@ -1,5 +1,5 @@
 import { assertNever } from "@grancalavera/ttt-core";
-import { GamePlaying, GameStatus, Move, Position, Token } from "./generated/graphql";
+import { GamePlayingState, GameState, Move, Position, Token } from "./generated/graphql";
 
 export type CellState = FreeCell | PlayedCell | DisabledCell;
 
@@ -17,7 +17,7 @@ type DisabledCell = {
   kind: "disabled";
 };
 
-export const updateBoard = (gameState: GameStatus): CellState[] => {
+export const updateBoard = (gameState: GameState): CellState[] => {
   if (!gameState.__typename) {
     throw new Error("__typename is required");
   }
@@ -25,12 +25,12 @@ export const updateBoard = (gameState: GameStatus): CellState[] => {
   const select = selectCell(getPlayedCellByIndex(gameState.moves));
 
   switch (gameState.__typename) {
-    case "GamePlaying":
+    case "GamePlayingState":
       return amINext(gameState)
         ? freeBoard(gameState.me).map(select)
         : EMPTY_BOARD.map(select);
-    case "GameDraw":
-    case "GameWon":
+    case "GameOverDrawState":
+    case "GameOverWonState":
       return EMPTY_BOARD.map(select);
     default:
       return assertNever(gameState.__typename);
@@ -44,14 +44,14 @@ type SelectCell = <T extends FreeCell | DisabledCell>(
 type Index = number;
 type Maybe<T> = T | undefined;
 
-const selectCell: SelectCell = findCell => (cell, i) => findCell(i) ?? cell;
+const selectCell: SelectCell = (findCell) => (cell, i) => findCell(i) ?? cell;
 
 const BOARD = [...Array(9)].map((_, i) => i);
 
 const EMPTY_BOARD = BOARD.map(() => ({ kind: "disabled" as const }));
 
 const freeBoard = (token: Token): FreeCell[] =>
-  BOARD.map(i => ({
+  BOARD.map((i) => ({
     kind: "free",
     move: { position: indexToPosition(i), token },
   }));
@@ -59,12 +59,12 @@ const freeBoard = (token: Token): FreeCell[] =>
 const getPlayedCellByIndex = (moves: readonly Move[]) => (
   i: number
 ): Maybe<PlayedCell> => {
-  const move = moves.find(move => move.position === indexToPosition(i));
+  const move = moves.find((move) => move.position === indexToPosition(i));
   return move ? { kind: "played", move } : undefined;
 };
 
 const indexToPosition = (i: number): Position => String.fromCharCode(65 + i) as Position;
 
-const amINext = (game: GamePlaying) => game.next === game.me;
-export const amIWaiting = (game: GameStatus) =>
-  game.__typename === "GamePlaying" && !amINext(game);
+const amINext = (game: GamePlayingState) => game.next === game.me;
+export const amIWaiting = (game: GameState) =>
+  game.__typename === "GamePlayingState" && !amINext(game);
