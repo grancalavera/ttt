86762292c5a0db9a76f64ae66deb5ff1d995a5ec
Leon Coto <leoncoto@gmail.com>
2020-04-26 14:53:41 +0100

    make combine typesafe and total

--- ttt-domain/src/validation-result/validation.ts
+++ ttt-domain/src/validation-result/validation.ts
@@ -25,31 +25,26 @@ export const isInvalid = <T extends unknown, E extends unknown>(
 export const combine = <T extends unknown, E extends unknown>(
   results: Validation<T, E>[],
   merge: (l: T, r: T) => T = pickLatest
-): Validation<T, E> => {
-  return results.reduce((combined, result) => {
+): Validation<T, E> =>
+  results.reduce((combined, result) => {
     if (isEmpty(combined)) {
       return result;
     }
 
-    if (isInvalid(combined) && isInvalid(result)) {
-      return invalid([...combined.errors, ...result.errors]);
+    if (isInvalid(combined)) {
+      if (isInvalid(result)) {
+        return invalid([...combined.errors, ...result.errors]);
+      } else {
+        return combined;
+      }
+    } else {
+      if (isInvalid(result)) {
+        return result;
+      } else {
+        return valid(merge(combined.data, result.data));
+      }
     }
-
-    if (isValid(combined) && isInvalid(result)) {
-      return result;
-    }
-
-    if (isInvalid(combined) && isValid(result)) {
-      return combined;
-    }
-
-    if (isValid(combined) && isValid(result)) {
-      return valid(merge(combined.data, result.data));
-    }
-
-    throw new Error("validation.combine: a combination wasn't matched");
   }, invalid([]));
-};
 
 const pickLatest = <T extends unknown>(_: T, latest: T) => latest;
 
