0b068770773e835e648df960b2e834c01c83467e
Leon Coto <leoncoto@gmail.com>
2020-04-30 21:19:09 +0100

    basic api design

--- ttt-domain/src/index.ts
+++ ttt-domain/src/index.ts
@@ -1,2 +1,15 @@
+import * as game from "game";
+import * as move from "move";
+import { ValidateMove } from "move";
+import { invalidInput, isInvalid } from "validation";
+
 export { state as gameState, validate as validateGame } from "game";
-export * from "model";
+
+export const validateMove: ValidateMove = (input) =>
+  isInvalid(game.validate(input.game))
+    ? invalidMoveOnInvalidGame(input)
+    : move.validate(input);
+
+const invalidMoveOnInvalidGame: ValidateMove = invalidInput(
+  "Cannot play move on an invalid game"
+);
