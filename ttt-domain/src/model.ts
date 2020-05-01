d49be6801d57f97a751b4ed57d5c42204a982e2b
Leon Coto <leoncoto@gmail.com>
2020-05-01 14:31:10 +0100

    notes from call with J

--- ttt-domain/src/model.ts
+++ ttt-domain/src/model.ts
@@ -1,3 +1,6 @@
+// aggregates have a root object and a graph
+// of dependencies
+
 export interface Game {
   players: Players;
   moves: Move[];
@@ -24,5 +27,7 @@ export type Players = [Player, Player];
 export type Move = [Player, Position];
 export type Winner = [Player, Position[]];
 
-export type Player = string;
+export type Player = ID;
 export type Position = number;
+
+type ID = string;
