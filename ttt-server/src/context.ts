42c631a69278c9535738717abafbb660f2ceec17
Leon Coto <leoncoto@gmail.com>
2020-05-09 16:02:38 +0100

    remove dep on ttt-api

--- ttt-server/src/context.ts
+++ ttt-server/src/context.ts
@@ -2,12 +2,10 @@ import { Request, Response } from "express";
 import { SecureResolver } from "./auth";
 import { UsersDataSource } from "data-sources/users";
 import { GamesDataSource } from "data-sources/games";
-import { ITTTAPIDataSource } from "data-sources/ttt-api";
 
 export interface DataSources {
   users: UsersDataSource;
   games: GamesDataSource;
-  api: ITTTAPIDataSource;
 }
 
 export type DataSourceContext = WithRequestResponse & WithSecureResolver;
