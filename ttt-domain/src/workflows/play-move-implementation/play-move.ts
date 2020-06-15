d7c93afb8ba0a89c06df06088abcac6f87a472ee
Leon Coto <leoncoto@gmail.com>
2020-06-15 12:23:17 +0100

    remove module for apply state transition

--- ttt-domain/src/workflows/play-move-implementation/play-move.ts
+++ ttt-domain/src/workflows/play-move-implementation/play-move.ts
@@ -1,3 +1,4 @@
+import { Game, Move, OpenGame, Player, Players, Winner } from "../..";
 import { failure, getFailure, isFailure, success } from "../../result";
 import { InvalidInput, isInvalid, sequence } from "../../validation";
 import {
@@ -6,12 +7,12 @@ import {
   GameUpdateFailedError,
   PlayMove,
 } from "../play-move";
-import { applyStateTransition } from "./apply-state-transition";
 import { validateGameStatusIsOpen } from "./validate-game-status-is-open";
 import { validateIsPlayersTurn } from "./validate-is-players-turn";
 import { validatePlayerExistsInGame } from "./validate-player-exists-in-game";
 import { validatePositionInsideBoard } from "./validate-position-inside-board";
 import { validatePositionNotPlayed } from "./validate-position-not-played";
+import { findWinner } from "./winners";
 
 export const playMove: PlayMove = (dependencies) => (input) => async () => {
   const { findGame, updateGame } = dependencies;
@@ -40,6 +41,10 @@ export const playMove: PlayMove = (dependencies) => (input) => async () => {
   return success(updatedGame);
 };
 
+export const failWithMoveValidationError = (
+  validationResults: InvalidInput<CreateMoveInput>[]
+) => failure(new CreateMoveValidationError(validationResults));
+
 const validate = (input: CreateMoveInput) =>
   sequence([
     validateGameStatusIsOpen(input),
@@ -49,6 +54,31 @@ const validate = (input: CreateMoveInput) =>
     validatePositionInsideBoard(input),
   ]);
 
-export const failWithMoveValidationError = (
-  validationResults: InvalidInput<CreateMoveInput>[]
-) => failure(new CreateMoveValidationError(validationResults));
+const applyStateTransition = (input: CreateMoveInput): Game => {
+  const { game, player, playerPosition } = input;
+  const move: Move = [player, playerPosition];
+
+  if (game.status.kind === "DrawGame" || game.status.kind === "WonGame") {
+    return game;
+  }
+
+  const moves = [...game.moves, move];
+  const maybeWinner: Winner | undefined = findWinner(game.size, moves);
+
+  if (maybeWinner) {
+    return { ...game, moves, status: { kind: "WonGame", winner: maybeWinner } };
+  }
+
+  if (!maybeWinner && moves.length === game.size * game.size) {
+    return { ...game, moves, status: { kind: "DrawGame" } };
+  }
+
+  return {
+    ...game,
+    moves,
+    status: { kind: "OpenGame", next: getNextPlayer(game.status, game.players) },
+  };
+};
+
+const getNextPlayer = ({ next: last }: OpenGame, [p1, p2]: Players): Player =>
+  last === p1 ? p2 : p1;
