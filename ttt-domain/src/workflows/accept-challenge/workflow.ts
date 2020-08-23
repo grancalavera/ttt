37b891d3a64ff3fc52dcbb6f75d306e4ba78216d
Leon Coto <leoncoto@gmail.com>
2020-08-23 20:42:24 +0100

    fix workflow type for accept challenge

--- ttt-domain/src/workflows/accept-challenge/workflow.ts
+++ ttt-domain/src/workflows/accept-challenge/workflow.ts
@@ -1,15 +1,20 @@
-import { Match, Player, SystemConfig } from "../../domain/model";
+import { Match, Player, SystemConfig, MatchId } from "../../domain/model";
 import { UpsertMatch, WorkflowResult, FindMatch } from "../support";
 
 export type AcceptChallengeWorkflow = (
   dependencies: SystemConfig & FindMatch & UpsertMatch
 ) => AcceptChallenge;
 
-export type AcceptChallenge = (input: Player) => WorkflowResult<Match>;
+export type AcceptChallenge = (input: AcceptChallengeInput) => WorkflowResult<Match>;
+
+export interface AcceptChallengeInput {
+  matchId: MatchId;
+  player: Player;
+}
 
 export class AcceptChallengeError {
   readonly kind = "AcceptChallengeError";
-  constructor(readonly input: Player) {}
+  constructor(readonly input: AcceptChallengeInput) {}
 }
 
 declare module "../errors" {
