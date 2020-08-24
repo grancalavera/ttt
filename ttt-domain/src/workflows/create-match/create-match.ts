5692b082462db8f69bcc6df2f6e3628b2a3138a6
Leon Coto <leoncoto@gmail.com>
2020-08-24 19:10:55 +0100

    implement create match

--- ttt-domain/src/workflows/create-match/create-match.ts
+++ ttt-domain/src/workflows/create-match/create-match.ts
@@ -1,5 +1,22 @@
-import { CreateMatchWorkflow } from "./workflow";
+import { CreateMatchWorkflow, TooManyActiveMatchesError } from "./workflow";
+import { failure, success, isFailure, isSuccess } from "@grancalavera/ttt-etc";
+import { UnknownError } from "../support";
+import { Match } from "../../domain/model";
 
-export const createMatchWorkflow: CreateMatchWorkflow = (dependencies) => (input) => {
-  throw new Error("CreateMatchWorkflow not implemented");
+export const createMatchWorkflow: CreateMatchWorkflow = (dependencies) => async (
+  player
+) => {
+  const { getUniqueId, upsertMatch, countActiveMatches, maxActiveMatches } = dependencies;
+
+  const match: Match = { id: getUniqueId(), owner: player, state: { kind: "New" } };
+
+  const activeMatches = await countActiveMatches(player);
+
+  if (maxActiveMatches <= activeMatches) {
+    return failure(new TooManyActiveMatchesError(player, maxActiveMatches));
+  }
+
+  const result = await upsertMatch(match);
+
+  return isSuccess(result) ? success(match) : result;
 };
