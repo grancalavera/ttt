9af585226c9f180e180ab42f26a43ccf1a34b85a
Leon Coto <leoncoto@gmail.com>
2020-08-31 19:42:32 +0100

    collect coverage from all relevant files

--- ttt-domain/jest.config.js
+++ ttt-domain/jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
-  modulePaths: ["<rootDir>/src"]
+  modulePaths: ["<rootDir>/src"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/command/**/*.ts", "src/workflow/**/*.ts"],
 };
