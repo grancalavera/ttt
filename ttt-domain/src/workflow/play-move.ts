ed6d3e74e7988a584ec0994afc634eb2d82234fa
Leon Coto <leoncoto@gmail.com>
2020-08-31 20:10:24 +0100

    move all play move code into workflow file

--- ttt-domain/src/workflow/play-move.ts
+++ ttt-domain/src/workflow/play-move.ts
@@ -1,9 +1,17 @@
-import { failure, isFailure, success, isSome } from "@grancalavera/ttt-etc";
+import {
+  failure,
+  isFailure,
+  isSome,
+  none,
+  Option,
+  some,
+  success,
+} from "@grancalavera/ttt-etc";
 import { IllegalMoveError, UnknownPlayerError, WrongTurnError } from "../domain/error";
-import { Match, MatchState, Moves } from "../domain/model";
+import { Match, MatchState, Moves, Player, Position, Winner } from "../domain/model";
 import { domainFailure } from "../domain/result";
+import { columns, diagonals, rows } from "../system/board";
 import { PlayMoveWorkflow } from "./support";
-import { findWinner } from "./find-winner";
 
 export const playMove: PlayMoveWorkflow = (dependencies) => async (input) => {
   const { upsertMatch, gameSize } = dependencies;
@@ -43,4 +51,33 @@ export const playMove: PlayMoveWorkflow = (dependencies) => async (input) => {
   const match: Match = { matchDescription, matchState };
   const upsertResult = await upsertMatch(match);
   return isFailure(upsertResult) ? domainFailure(upsertResult) : success(match);
+};
+
+const findWinner = (size: number, moves: Moves): Option<Winner> => {
+  const winSequences = [...rows(size), ...columns(size), ...diagonals(size)];
+  const findPlayer = playerFinder(moves);
+
+  let winnerOption: Option<Winner> = none;
+
+  for (const winSequence of winSequences) {
+    const players = winSequence.map(findPlayer);
+    const first = players[0] ?? none;
+
+    if (
+      // first move in the win sequence was played
+      isSome(first) &&
+      // every move in the sequence was played by the same player
+      players.every((o) => isSome(o) && o.value.id === first.value.id)
+    ) {
+      winnerOption = some([first.value, winSequence]);
+      break;
+    }
+  }
+
+  return winnerOption;
+};
+
+const playerFinder = (moves: Moves) => (position: Position): Option<Player> => {
+  const move = moves.find(([, p]) => p === position);
+  return move ? some(move[0]) : none;
 };
