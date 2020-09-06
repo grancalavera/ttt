5b1281fd5ca629c91bf2b8998fc6396ceb06ace5
Leon Coto <leoncoto@gmail.com>
2020-09-06 12:55:56 +0100

    model pipeline

--- ttt-domain/src/main.ts
+++
@@ -1,4 +0,0 @@
-export * from "./domain/model";
-export { createChallenge } from "./workflow/create-challenge";
-export { createGame } from "./workflow/create-game";
-export { createMatch } from "./workflow/create-match";
--- ttt-etc/src/assert-never.ts
+++ ttt-etc/src/assert-never.ts
@@ -1,3 +1,7 @@
 export function assertNever(never: never, message?: string) {
   throw new Error(message ?? `got ${never} but expected never`);
 }
+
+export function softAssertNever(never: never, message?: string) {
+  console.error(message ?? `got ${never} but expected never`);
+}
